61bdc78e7616d5ca28809dea6fe7f9248a0c76d7
alien-wxr <alien-wxr@outlook.com>
2020-07-08 00:14:39 +0800

    create the project

---
+++ PentaZen iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PentaZen iOS
+//
+//  Created by 王小荣 on 2020/7/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PentaZen iOS/PentaZen_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentaZen_iOSApp.swift
+//  PentaZen iOS
+//
+//  Created by 王小荣 on 2020/7/7.
+//
+
+import SwiftUI
+
+@main
+struct PentaZen_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
